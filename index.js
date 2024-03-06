6da8a3807bbd0f8770ef34f565a792b839d85209
Arbaaz Khan <arbaazkhanark23@gmail.com>
2024-03-06 12:19:04 +0530

    cursor-style

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const cursorDot=document.querySelector("[data-cursor-dot]");
+const cursorOutline=document.querySelector("[data-cursor-outline]");
+
+console.log(cursorDot)
+
+window.addEventListener('mousemove',(e)=>{
+    const posX=e.clientX;
+    const posY=e.clientY;
+
+    cursorDot.style.left=`${posX}px`;
+    cursorDot.style.top=`${posY}px`;
+
+    cursorOutline.style.left=`${posX}px`;
+    cursorOutline.style.top=`${posY}px`;
+
+    cursorOutline.animate({
+        left:`${posX}px`,
+        top:`${posY}px`
+    },{duration:500,fill:"forwards"});
+})
+
+
+
+
 const options = document.getElementsByTagName('option');
 
 for (let i = 0; i < options.length; i++) {
